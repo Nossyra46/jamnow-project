c35a762859c00ff379e9775461cdaa397f205e87
Aziosmanoff <nine.aziosmanoff@club.fr>
2016-09-06 17:35:25 +0200

    responsive menu

--- src/public/assets/javascript/script.js
+++ src/public/assets/javascript/script.js
@@ -1,6 +1,8 @@
-// MENU RESPONSIVE MATERIALIZE
-console.log("le js fonctionne");
-  // Initialize collapse button
+  $(document).ready(function(){
+  	  // Initialize collapse button
   $(".button-collapse").sideNav();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   //$('.collapsible').collapsible();
+  });
+
+
