c5ff146098c77a9797d025dd1e2d32312699a101
Nossyra46 <nossyra@gmail.com>
2016-09-06 14:57:47 +0200

    redirection successful, next printing the right datas

--- src/public/app/controllers/JamController.js
+++ src/public/app/controllers/JamController.js
@@ -1,33 +1,12 @@
-jamApp.controller('JamController', ['$scope', '$http', 'ApiFactory', function($scope, $http, ApiFactory) {
-  this.name = '';
-  this.description = '';
-  this.date = '';
-  this.hour = '';
-  this.location = '';
-  this.limit = '';
-
-  this.addJam = function() {
-    // Creating a Jam object with all the right properties filled by the user
-    var jam = {
- 			name: this.name,
-      description: this.description,
-      date: this.date,
- 			hour: this.hour,
- 			location: this.location,
-      limit: this.limit
- 		};
-
- 		// Sending the object to the API via the POST method
-
- 		$http.post(ApiFactory.api+'jams', jam)
- 		.then(function(response){
- 			this.response = 'omg you did it !';
- 			console.log(this.response);
-      console.log(response.data._id);
-      var id = response.data._id;
- 		}, function(err) {
- 			this.response = 'ONOES';
- 			console.log(this.response);
- 		});
-  };
+jamApp.controller('JamController', ['$scope', '$http', '$stateParams', 'ApiFactory', function($scope, $http, $stateParams, ApiFactory) {
+  $http.get(ApiFactory.api + 'jams/' + $stateParams.id_jam)
+  .then(
+    function(response) {
+      console.log(response.data);
+    },
+    function(err) {
+      // body...
+      console.log('Unable to retrieve data from the api');
+    }
+    );
 }]);
