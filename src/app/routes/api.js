23e9e4b7b7950eb7243ca53ff6255ddacdd867a3
Nossyra46 <nossyra@gmail.com>
2016-09-07 16:17:27 +0200

    typhus soigné

--- src/app/routes/api.js
+++ src/app/routes/api.js
@@ -43,7 +43,7 @@ passport.serializeUser(function(user, cb) {
 });
 
 passport.deserializeUser(function(id, cb) {
-  db.User.findById(id, function (err, user) {
+  User.findById(id, function (err, user) {
     if (err) { return cb(err); }
     cb(null, user);
   });
