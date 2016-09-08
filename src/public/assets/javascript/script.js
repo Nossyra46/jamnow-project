6681ab10a079bb790738197574d9998f60894377
Aziosmanoff <nine.aziosmanoff@club.fr>
2016-09-08 14:32:43 +0200

    card jam profil + calendarpicker

--- src/public/assets/javascript/script.js
+++ src/public/assets/javascript/script.js
@@ -1,8 +1,14 @@
+  						// NAVBAR
   $(document).ready(function(){
   	  // Initialize collapse button
   $(".button-collapse").sideNav();
   // Initialize collapsible (uncomment the line below if you use the dropdown variation)
   //$('.collapsible').collapsible();
   });
-
-
+  					// DATEPICKER
+  $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15 // Creates a dropdown of 15 years to control year
+  });
+      
+  
