b4b0bff09c054743b7b3a0d3be910c41a3f93e98
Aziosmanoff <nine.aziosmanoff@club.fr>
2016-09-08 15:00:07 +0200

    update master

--- src/public/assets/javascript/script.js
+++ src/public/assets/javascript/script.js
@@ -8,7 +8,5 @@
   					// DATEPICKER
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
+    selectYears: 10 // Creates a dropdown of 15 years to control year
   });
-      
-  
