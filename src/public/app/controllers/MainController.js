87e5d8ae14510bd833a8594c1e3f67a66fba394e
Nossyra46 <nossyra@gmail.com>
2016-09-09 17:24:37 +0200

    connecté

--- src/public/app/controllers/MainController.js
+++ src/public/app/controllers/MainController.js
@@ -1,10 +1,14 @@
 jamApp.controller('MainController', ['$scope', 'LoginFactory', function($scope, LoginFactory) {
-	this.logged = LoginFactory.isLogged;
-	console.log("I'm loaded");
+	var self = this;
+	$scope.$on('loginEvent', function(event, data) {
+		self.logged = LoginFactory.isLogged;
+		console.log("I'm loaded");
+	});
 	this.logout = function() {
 		// body...
 		LoginFactory.user = {};
 		LoginFactory.isLogged = false;
+		self.logged = LoginFactory.isLogged;
 		Materialize.toast("Vous vous êtes déconnecté.", 3000);
 	}
 }]);
